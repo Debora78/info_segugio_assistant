490b6ba1c7f6b87c0be502b4f956ece31743a60e
Debora Campopiano <programmazione2023@gmail.com>
2025-06-04 11:26:25 +0200

    start project info_segugio_assistant

---
+++ README.md
@@ -0,0 +1,13 @@
+- ENTRO NELLA CARTELLA
+## cd info_segugio_assistant
+
+- INSTALLARE LA LIBRERIA POETRY
+## poetry install
+
+- ATTIVARE VIRTUAL ENVIRONMENT
+## eval $(poetry env activate)
+- ATTIVARE VIRTUAL ENVIRONMENT SU WINDOW ( i percorsi di Windows vanno trattati con gli slash / invece dei backslash \ e le lettere dei drive (C:) vanno scritte in minuscolo con /c/.)
+## source /c/Users/Windows10/AppData/Local/pypoetry/Cache/virtualenvs/info-segugio-assistant-bdREUtaS-py3.13/Scripts/activate
+
+- LANCIARE L'APPLICATIVO(src perché è il percorso dove poetry mi ha creato il progetto )
+## chainlit run src/info_segugio_assistant/__init__.py -w
---
+++ src/info_segugio_assistant/__init__.py
@@ -0,0 +1,16 @@
+# inizializzare il sistema
+# importa la libreria Chainlit con nome abbreviato 'cl'
+
+import chainlit as cl
+
+# con questo decoratore diciamo che ogni volta che viene ricevuto un messaggio dall'interfaccia, viene chiamata la funzione main
+# in message metti le informazioni che l'utente ha scritto 
+@cl.on_message
+async def main(message: cl.Message):
+    # Your custom logic goes here...
+
+    # Send a response back to the user
+    # Con questo comando riesce ad inviare all'interfaccia chainlit una risposta che ripropone il messaggio dell'utente
+    await cl.Message(
+        content=f"Received: {message.content}",
+    ).send()
