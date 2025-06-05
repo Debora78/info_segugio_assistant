45895af9394a2603cdb10ec6177de1228c8c6154
Debora Campopiano <programmazione2023@gmail.com>
2025-06-05 15:32:17 +0200

    commentato il config.py

--- src/info_segugio_assistant/config.py
+++ src/info_segugio_assistant/config.py
@@ -8,8 +8,8 @@ import os
 # AI_API_KEY = "ollama"
 
 ### LLM su OpenAI
-class Config:
-    LLM_MODEL = "gpt-4o" # 
-    LLM_MODEL_LOW = "gpt-4o-mini" 
-    AI_API_URL = "https://api.openai.com/v1"
-    AI_API_KEY = "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"
+# class Config:
+#     LLM_MODEL = "gpt-4o" # 
+#     LLM_MODEL_LOW = "gpt-4o-mini" 
+#     AI_API_URL = "https://api.openai.com/v1"
+#     AI_API_KEY = "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"
